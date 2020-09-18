bb02516e936b62468cd93d3e132b5bb493fa7e92
Gerome <gerome@jemstep.com>
2020-09-18 15:38:47 +0200

    Added SSH from anywhere

--- 3. IAM & EC2/15. EC2 Introduction/main.tf
+++ 3. IAM & EC2/15. EC2 Introduction/main.tf
@@ -20,6 +20,30 @@ resource "aws_default_vpc" "default_vpc" {
     }
 }
 
+resource "aws_security_group" "ssh_group" {
+    name = "allow_ssh"
+    description = "Allow us to SSH into EC2 instances"
+    vpc_id = aws_default_vpc.default_vpc.id
+
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "tcp"
+        from_port = 22
+        to_port = 22
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow_ssh"
+    }
+}
+
 data "aws_ami" "aws_linux" {
     owners = [ "amazon" ]
     name_regex = "amazon_linux_2"
@@ -29,6 +53,7 @@ data "aws_ami" "aws_linux" {
 resource "aws_instance" "ec2" {
     ami = data.aws_ami.aws_linux.id
     instance_type = "t2.micro"
+    security_groups = [ aws_security_group.ssh_group.name ]
 
     tags = {
         Name = "gerome-dev-sa-ec2-intro"
